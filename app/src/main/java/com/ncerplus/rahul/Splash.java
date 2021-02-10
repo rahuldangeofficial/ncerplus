3373df11064f69a0468c8e3d4ea02158910f706e
Rahul Dange <rahuldangeofficial@gmail.com>
2021-02-10 21:37:53 +0530

    Basic Build

---
+++ app/src/main/java/com/ncerplus/rahul/Splash.java
@@ -0,0 +1,37 @@
+package com.ncerplus.rahul;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
